ed6e2d0a2d8c7cc4ef5f99fa925e4113f0e6caa9
rlpacht <rl.pacht@gmail.com>
2015-09-19 12:33:03 -0700

    trying to make httparty work

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,27 +1,28 @@
 require 'net/http'
 # require 'httparty'
 class RecipesController < ApplicationController
-  
+
   include HTTParty
-  base_uri("http://api.yummly.com/v1/recipes?")
+  base_uri("http://api.yummly.com/v1/api")
 
 	def index
-    # include HTTParty
-    # base_uri("http://api.yummly.com/v1")
-		p 'INSIDE INDEX'
+    p 'INSIDE INDEX'
+    food_searched = params[:query]
+    search(food_searched)
 	end
 
-  def search
-    # food_searched = params[:food]
-    food_searched = "chicken"
-    query = {query: {
+  private 
+
+  def search(search_query)
+    # food_searched = "chicken"
+    query_params = {query: {
       _app_id: ENV["yummly_app_id"], 
       _app_key: ENV["yummy_app_key"], 
-      q: food_searched, 
-      max_results: 30
+      q: search_query 
+      # max_results: 30
     }}
+    response = self.class.get("recipes", query_params)
 
-    p query
-    
+    p response
   end
 end
