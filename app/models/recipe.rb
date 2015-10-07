e77d64921ab8b67e93d83cab5ec80a315838b11f
rlpacht <rl.pacht@gmail.com>
2015-10-06 22:49:18 -0700

    use as_json instead of to_json

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,6 +4,6 @@ class Recipe < ActiveRecord::Base
   # has_many :users through: :user_favorites
 
   def json_with_note_ids
-    JSON.parse(to_json({methods: :note_ids}))
+    as_json({methods: :note_ids})
   end
 end
